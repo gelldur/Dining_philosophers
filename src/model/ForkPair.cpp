e0f3a51e89512388f8fb80a7cd61c474fa827c51
Dawid Drozd <drozddawid.uam@gmail.com>
2017-01-22 18:53:22 +0100

    Add ForkPair

---
+++ src/model/ForkPair.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Dawid Drozd aka Gelldur on 22/01/17.
+//
+#include "ForkPair.h"
+//////////////////////////////////
+
+void ForkPair::startUse()
+{
+
+}
+
+void ForkPair::stopUse()
+{
+
+}
---
+++ src/model/ForkPair.h
@@ -0,0 +1,13 @@
+//
+// Created by Dawid Drozd aka Gelldur on 22/01/17.
+//
+#pragma once
+
+class ForkPair
+{
+public:
+	void startUse();
+	void stopUse();
+};
+
+
